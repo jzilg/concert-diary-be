cb635984d8c6f724b9c1e9df4a29d6b306393912
jzilg <jzg@dasburo.com>
2020-04-03 17:19:16 +0200

    remove unused resource

--- src/app/Http/Controllers/ConcertController.php
+++ src/app/Http/Controllers/ConcertController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Laravel\Lumen\Http\Response;
-use Laravel\Lumen\Http\ResponseFactory;
 use App\Concert;
 
 class ConcertController extends Controller
